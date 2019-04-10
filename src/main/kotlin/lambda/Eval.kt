f2437a989a6fdc9b63d61e67c922e5f698c8250b
David Petersen <david@familiep.de>
2019-04-10 13:39:31 +0200

    implement substitution

--- src/main/kotlin/lambda/Eval.kt
+++ src/main/kotlin/lambda/Eval.kt
@@ -1,5 +1,16 @@
 package lambda
 
+object Eval {
+
+    fun substitute(scrutinee: Ident, replacement: Expression, expr: Expression): Expression{
+        return when(expr){
+            is Var -> if(expr.ident == scrutinee) replacement else expr
+            is Lamdba -> if(expr.binder != scrutinee) Lamdba(expr.binder, substitute(scrutinee, replacement, expr.body)) else expr
+            is App -> App(substitute(scrutinee, replacement, expr.func), substitute(scrutinee, replacement, expr.arg))
+        }
+    }
+}
+
 fun Expression.freeVars(): Set<Ident>{
     return when(this){
         is Var -> hashSetOf(ident)
--- src/test/kotlin/lambda/EvalKtTest.kt
+++ src/test/kotlin/lambda/EvalKtTest.kt
@@ -6,9 +6,42 @@ import org.junit.Assert.*
 
 class EvalKtTest {
 
+    @Test
+    fun `should substitute var`(){
+        stc("x", "y", "x", "y")
+    }
 
     @Test
-    fun freeVars() {
+    fun `should not  substitute var`() {
+        stc("k", "y", "x", "x")
+    }
+
+    @Test
+    fun `should substitute in app`(){
+        stc("x", "y", "x x", "y y")
+        stc("x", "\\x.x", "x (y x)", "(\\x.x) (y \\x.x)")
+    }
+
+    @Test
+    fun `should substitute in lambda`(){
+        stc("x", "y", "\\k.x", "\\k.y")
+    }
+
+    @Test
+    fun `should not substitute in lambda`(){
+        stc("x", "y", "\\x.x", "\\x.x")
+    }
+
+    private fun stc(scrutinee: String, replacement: String, expr: String, expected: String){
+        val scrutinee_ = Ident(scrutinee)
+        val replacement_ = Parser(Lexer(replacement)).parseExpression()
+        val expr_ = Parser(Lexer(expr)).parseExpression()
+        val expected_= Parser(Lexer(expected)).parseExpression()
+        assertEquals(expected_, Eval.substitute(scrutinee_, replacement_, expr_))
+    }
+
+    @Test
+    fun `should return free vars`() {
         fvtc("x y z", listOf("x", "y", "z"))
         fvtc("\\x.x z", listOf("z"))
         fvtc("(\\x.x) x y z", listOf("x", "y", "z"))

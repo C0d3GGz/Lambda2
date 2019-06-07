6d9cb7bcba5837166659ea2466a654d70a7446e5
kRITZCREEK <christoph.hegemann1337@gmail.com>
2019-06-07 09:11:59 +0200

    wip

--- src/main/kotlin/lambda/Typechecker.kt
+++ src/main/kotlin/lambda/Typechecker.kt
@@ -2,9 +2,43 @@ package lambda
 
 import io.vavr.kotlin.*
 import io.vavr.collection.HashMap
+import io.vavr.control.Option
 
 typealias TCContext = HashMap<Ident, Scheme>
-typealias Substitution = HashMap<Ident, Type>
+
+class Substitution(val subst: HashMap<Ident, Type>) {
+
+    fun get(ident: Ident): Option<Type> {
+        return subst.get(ident)
+    }
+
+    fun apply(ty: Type): Type {
+        return when (ty) {
+            is Type.Int -> ty
+            is Type.Bool -> ty
+            is Type.Fun -> Type.Fun(this.apply(ty.arg), this.apply(ty.result))
+            is Type.Var -> this.get(ty.ident).getOrElse(ty)
+        }
+    }
+
+//    fun removeAll(idents: List<Ident>): Substitution {
+//        return Substitution(subst.removeAll(idents))
+//    }
+//
+//    fun apply(scheme: Scheme): Scheme {
+//        val tmpSubst = Substitution(this.subst.removeAll(scheme.vars))
+//        return Scheme(scheme.vars, tmpSubst.apply(scheme.ty))
+//    }
+//
+//    fun apply(ctx: TCContext): TCContext {
+//        return ctx.mapValues {
+//            this.apply(it)
+//        }
+//    }
+
+}
+
+val emptySubstitution = Substitution(hashMap())
 
 class Typechecker {
 
@@ -15,41 +49,21 @@ class Typechecker {
         return Type.Var(Ident(fresh.toString()))
     }
 
-    fun applyType(subst: Substitution, ty: Type): Type {
-        return when (ty) {
-            is Type.Int -> ty
-            is Type.Bool -> ty
-            is Type.Fun -> Type.Fun(applyType(subst, ty.arg), applyType(subst, ty.result))
-            is Type.Var -> subst.get(ty.ident).getOrElse(ty)
-        }
-    }
-
-    fun applyScheme(subst: Substitution, scheme: Scheme): Scheme {
-        val tmpSubst = subst.removeAll(scheme.vars)
-        return Scheme(scheme.vars, applyType(tmpSubst, scheme.ty))
-    }
-
-    fun applyContext(subst: Substitution, ctx: TCContext): TCContext {
-        return ctx.mapValues {
-            applyScheme(subst, it)
-        }
-    }
-
     fun infer(ctx: TCContext, expr: Expression): Pair<Substitution, Type> {
         return when (expr) {
             is Literal -> {
                 return when (expr.lit) {
-                    is IntLit -> Pair(hashMap(), Type.Int)
-                    is BoolLit -> Pair(hashMap(), Type.Bool)
+                    is IntLit -> Pair(emptySubstitution, Type.Int)
+                    is BoolLit -> Pair(emptySubstitution, Type.Bool)
                 }
             }
             is Lambda -> {
                 val tyBinder = freshVar()
                 val tmpCtx = ctx.put(expr.binder, Scheme(emptyList(), tyBinder))
                 val (s1, tyBody) = this.infer(tmpCtx, expr.body)
-                return Pair(s1, Type.Fun(applyType(s1, tyBinder), tyBody))
+                return Pair(s1, Type.Fun(s1.apply(tyBinder), tyBody))
             }
-            else -> Pair(hashMap(), Type.Int)
+            else -> Pair(emptySubstitution, Type.Int)
         }
     }
 }

b192f61fe4b37df59f960d7e520404617434a795
David Petersen <david@familiep.de>
2019-12-09 13:47:48 +0100

    finished closure part of runtime system

--- src/main/kotlin/lambda/backend/Codegen.kt
+++ src/main/kotlin/lambda/backend/Codegen.kt
@@ -1,11 +1,15 @@
 package lambda.backend
 
-import asmble.ast.Node
+import asmble.ast.Node.*
+import asmble.ast.Node.Func
+import asmble.ast.Node.Global
+import asmble.ast.Node.Type.*
 import asmble.io.AstToBinary
 import asmble.io.AstToSExpr
 import asmble.io.ByteWriter
 import java.io.ByteArrayOutputStream
 import java.io.File
+import kotlin.Exception
 
 fun main() {
     val module = Codegen().compileModule()
@@ -18,169 +22,312 @@ fun main() {
     File("out.wasm").writeBytes(buffer.toByteArray())
 }
 
-class Locals(vararg params: Node.Type.Value) {
-    private var list: MutableList<Node.Type.Value> = params.toMutableList()
+class Locals(vararg params: Value) {
+    private var list: MutableList<Value> = params.toMutableList()
     private var nParams: Int = params.size
 
-    fun register(type: Node.Type.Value): Int = list.size.also { list.add(type) }
+    fun register(type: Value): Int = list.size.also { list.add(type) }
 
     // All the locals the user registered
-    fun getLocals(): List<Node.Type.Value> = list.drop(nParams)
+    fun getLocals(): List<Value> = list.drop(nParams)
 }
 
 class Codegen() {
 
-    private val globalFuncs = mutableListOf<Pair<String, Node.Func>>()
+    private val globalFuncs = mutableListOf<Pair<String, Func>>()
 
-    private val globals = mutableListOf<Pair<String, Node.Global>>()
+    private val tableFuncs = mutableListOf<String>()
 
-    private fun registerGlobalFunc(name: String, func: Node.Func): Int =
-        globalFuncs.size.also { globalFuncs.add(name to func) }
+    private val globals = mutableListOf<Pair<String, Global>>()
 
-    private fun registerGlobals(name: String, global: Node.Global): Int =
+    private val types = mutableListOf<Pair<String, Type.Func>>()
+
+    private fun registerGlobalFunc(name: String, func: Func, isTable: Boolean): Int =
+        globalFuncs.size.also {
+            globalFuncs.add(name to func)
+            if (isTable) tableFuncs.add(name)
+        }
+
+    private fun registerGlobals(name: String, global: Global): Int =
         globals.size.also { globals.add(name to global) }
+
+    private fun registerType(name: String, type: Type.Func): Int =
+        types.size.also { types.add(name to type) }
+
 
     fun makeFunc1(
         name: String,
-        param: Node.Type.Value,
-        res: Node.Type.Value?,
-        body: (locals: Locals, paramIndex: Int) -> List<Node.Instr>
+        param: Value,
+        res: Value?,
+        isTable: Boolean = false,
+        body: (locals: Locals, paramIndex: Int) -> List<Instr>
     ): Int {
         val locals = Locals(param)
         val instrs = body(locals, 0)
-        val func = Node.Func(
-            type = Node.Type.Func(listOf(param), ret = res),
+        val func = Func(
+            type = Type.Func(listOf(param), ret = res),
             locals = locals.getLocals(),
             instructions = instrs
         )
-        return registerGlobalFunc(name, func)
+        return registerGlobalFunc(name, func, isTable)
     }
 
     fun makeFunc2(
         name: String,
-        param1: Node.Type.Value,
-        param2: Node.Type.Value,
-        res: Node.Type.Value?,
-        body: (Locals, Int, Int) -> List<Node.Instr>
+        param1: Value,
+        param2: Value,
+        res: Value?,
+        isTable: Boolean = false,
+        body: (Locals, Int, Int) -> List<Instr>
     ): Int {
         val locals = Locals(param1, param2)
         val instrs = body(locals, 0, 1)
-        val func = Node.Func(
-            type = Node.Type.Func(listOf(param1, param2), ret = res),
+        val func = Func(
+            type = Type.Func(listOf(param1, param2), ret = res),
             locals = locals.getLocals(),
             instructions = instrs
         )
-        return registerGlobalFunc(name, func)
+        return registerGlobalFunc(name, func, isTable)
     }
 
-    fun compileModule(): Node.Module {
+    fun compileModule(): Module {
         val watermark = registerGlobals(
             "watermark",
-            Node.Global(
-                Node.Type.Global(Node.Type.Value.I32, true),
-                listOf(Node.Instr.I32Const(0))
+            Global(
+                Type.Global(Value.I32, true),
+                listOf(Instr.I32Const(0))
             )
         )
 
         val allocate = makeFunc1(
             "allocate",
-            Node.Type.Value.I32, Node.Type.Value.I32
+            Value.I32, Value.I32
         ) { locals, bytes ->
-            val res = locals.register(Node.Type.Value.I32)
+            val res = locals.register(Value.I32)
 
             listOf(
-                Node.Instr.GetGlobal(watermark),
-                Node.Instr.TeeLocal(res), // Set- and GetLocal
-                Node.Instr.GetLocal(bytes),
-                Node.Instr.I32Add,
-                Node.Instr.SetGlobal(watermark),
-                Node.Instr.GetLocal(res)
+                Instr.GetGlobal(watermark),
+                Instr.TeeLocal(res), // Set- and GetLocal
+                Instr.GetLocal(bytes),
+                Instr.I32Add,
+                Instr.SetGlobal(watermark),
+                Instr.GetLocal(res)
             )
         }
 
         val makeClosure = makeFunc2(
             "make_closure",
-            Node.Type.Value.I32, Node.Type.Value.I32,
-            Node.Type.Value.I32
+            Value.I32, Value.I32,
+            Value.I32
         ) { locals, arity, code_pointer ->
-            val closure = locals.register(Node.Type.Value.I32)
+            val closure = locals.register(Value.I32)
 
             listOf(
 
                 // size of argument memory
-                Node.Instr.GetLocal(arity),
-                Node.Instr.I32Const(4), // 4 byte per argument (32 bit)
-                Node.Instr.I32Mul,
+                Instr.GetLocal(arity),
+                Instr.I32Const(4), // 4 byte per argument (32 bit)
+                Instr.I32Mul,
 
                 // size for static components of closure
-                Node.Instr.I32Const(8), // arity 2, applied 2, code_pointer 4
-                Node.Instr.I32Add,
+                Instr.I32Const(8), // arity 2, applied 2, code_pointer 4
+                Instr.I32Add,
 
-                Node.Instr.Call(allocate),
-                Node.Instr.SetLocal(closure),
+                Instr.Call(allocate),
+                Instr.SetLocal(closure),
 
                 // initialize arity
-                Node.Instr.GetLocal(closure),
-                Node.Instr.GetLocal(arity),
-                Node.Instr.I32Store16(1, 0),
+                Instr.GetLocal(closure),
+                Instr.GetLocal(arity),
+                Instr.I32Store16(1, 0),
 
                 // initialize applied arguments (to zero is redundant)
 
-                Node.Instr.GetLocal(closure),
-                Node.Instr.I32Const(4), // jump over arity and applied
-                Node.Instr.I32Add,
+                Instr.GetLocal(closure),
+                Instr.I32Const(4), // jump over arity and applied
+                Instr.I32Add,
 
-                Node.Instr.GetLocal(code_pointer),
-                Node.Instr.I32Store(2, 0),
+                Instr.GetLocal(code_pointer),
+                Instr.I32Store(2, 0),
 
-                Node.Instr.GetLocal(closure)
+                Instr.GetLocal(closure)
 
             )
 
+        }
+
+        val applyClosure = makeFunc2(
+            "apply_closure",
+            Value.I32,
+            Value.I32,
+            Value.I32
+        ) { locals, closure, arg ->
+            val applied_value = locals.register(Value.I32)
+            val applied_location = locals.register(Value.I32)
+
+
+            listOf(
+
+//          apply_closure(closure, arg){
+//              closure.args[applied] = arg
+//              if closure.applied+1 < closure.arity then
+//                  closure.applied++
+//                  closure
+//              else
+//                  closure.code_pointer(&closure.arguments)
+//          }
+
+                //jump to applied location
+                Instr.GetLocal(closure),
+                Instr.I32Const(2),
+                Instr.I32Add,
+
+                // read applied value
+                Instr.TeeLocal(applied_location),
+                Instr.I32Load16S(1, 0), //loads applied value onto stack
+                Instr.SetLocal(applied_value),
+
+                // jumping to start of argument vector
+                Instr.GetLocal(closure),
+                Instr.I32Const(8),
+                Instr.I32Add,
+
+                // jumping over applied arguments
+                Instr.GetLocal(applied_value),
+                Instr.I32Const(4),
+                Instr.I32Mul,
+                Instr.I32Add,
+
+                // write argument into argument vector
+                Instr.GetLocal(arg),
+                Instr.I32Store(2, 0),
+
+                //applied+1
+                Instr.GetLocal(applied_value),
+                Instr.I32Const(1),
+                Instr.I32Add,
+
+                //closure.arity
+                Instr.GetLocal(closure),
+                Instr.I32Load16S(1, 0),
+
+                //if argument list isn't complete yet
+                Instr.I32LtS,
+
+                Instr.If(Value.I32),
+
+                //store applied++ to closure.applied, return closure
+                Instr.GetLocal(applied_location),
+                Instr.GetLocal(applied_value),
+                Instr.I32Const(1),
+                Instr.I32Add,
+                Instr.I32Store16(1, 0),
+                Instr.GetLocal(closure),
+
+                Instr.Else,
+                // jumping to start of argument vector
+                Instr.GetLocal(closure),
+                Instr.I32Const(8),
+                Instr.I32Add,
+
+                // jumping to code_pointer
+                Instr.GetLocal(closure),
+                Instr.I32Const(4),
+                Instr.I32Add,
+                Instr.I32Load(2, 0),
+
+                Instr.CallIndirect(
+                    registerType(
+                        "int_to_int",
+                        Type.Func(listOf(Value.I32), Value.I32)
+                    ), false
+                ),
+
+                //end if
+                Instr.End
+
+            )
         }
 
         val add = makeFunc2(
             "add",
-            Node.Type.Value.I32, Node.Type.Value.I32, Node.Type.Value.I32
+            Value.I32, Value.I32, Value.I32
         ) { _, x, y ->
             listOf(
-                Node.Instr.GetLocal(x),
-                Node.Instr.GetLocal(y),
-                Node.Instr.I32Add
+                Instr.GetLocal(x),
+                Instr.GetLocal(y),
+                Instr.I32Add
             )
         }
 
-        val add3 = makeFunc1("add3", Node.Type.Value.I32, Node.Type.Value.I32) { locals, x ->
-            val y = locals.register(Node.Type.Value.I32)
+        val add3 = makeFunc1("add3", Value.I32, Value.I32) { locals, x ->
+            val y = locals.register(Value.I32)
             listOf(
-                Node.Instr.I32Const(3),
-                Node.Instr.SetLocal(y),
-                Node.Instr.GetLocal(x),
-                Node.Instr.GetLocal(y),
-                Node.Instr.Call(add)
+                Instr.I32Const(3),
+                Instr.SetLocal(y),
+                Instr.GetLocal(x),
+                Instr.GetLocal(y),
+                Instr.Call(add)
             )
         }
 
-        val fn0 = Node.Func(
-            type = Node.Type.Func(params = emptyList(), ret = Node.Type.Value.I32),
+        val duble = makeFunc2("duble\$inner", Value.I32, Value.I32, Value.I32, false) { _, x, y ->
+            listOf(
+                Instr.GetLocal(x),
+                Instr.GetLocal(y),
+                Instr.I32Add,
+                Instr.I32Const(2),
+                Instr.I32Mul
+            )
+        }
+
+        val dubleWrapper = makeFunc1("duble", Value.I32, Value.I32, true) { _, arg_location ->
+            listOf(
+                Instr.GetLocal(arg_location),
+                Instr.I32Load(2, 0),
+
+                Instr.GetLocal(arg_location),
+                Instr.I32Const(4),
+                Instr.I32Add,
+                Instr.I32Load(2, 0),
+
+                Instr.Call(duble)
+            )
+        }
+
+        val fn0 = Func(
+            type = Type.Func(params = emptyList(), ret = Value.I32),
             locals = emptyList(),
             instructions = listOf(
-                Node.Instr.I32Const(20),
-                Node.Instr.Call(allocate),
-                Node.Instr.Drop,
-                Node.Instr.GetGlobal(watermark)
+
+                Instr.I32Const(globalFuncs.find { it.first == "duble\$inner" }!!.second.type.params.size), //arity
+                Instr.I32Const(tableFuncs.indexOf("duble")), //code pointer
+                Instr.Call(makeClosure),
+
+                Instr.I32Const(21),
+                Instr.Call(applyClosure),
+
+                Instr.I32Const(22),
+                Instr.Call(applyClosure)
+
             )
         )
 
-        val main = registerGlobalFunc("main", fn0)
+        val main = registerGlobalFunc("main", fn0, false)
 
-        return Node.Module(
+        return Module(
             funcs = globalFuncs.map { it.second },
             exports = globalFuncs.mapIndexed { i, (name, _) ->
-                Node.Export(name, Node.ExternalKind.FUNCTION, i)
+                Export(name, ExternalKind.FUNCTION, i)
             },
             globals = globals.map { it.second },
-            memories = listOf(Node.Type.Memory(Node.ResizableLimits(1, null)))
+            memories = listOf(Memory(ResizableLimits(1, null))),
+            tables = listOf(Table(ElemType.ANYFUNC, ResizableLimits(globalFuncs.size, null))),
+            elems = listOf(Elem(0, listOf(Instr.I32Const(0)), tableFuncs.map {
+                globalFuncs.indexOfFirst { (name, _) -> name == it }.apply {
+                    if (this == -1) throw Exception("unknown table fun $it")
+                }
+            }))
         )
     }
 }

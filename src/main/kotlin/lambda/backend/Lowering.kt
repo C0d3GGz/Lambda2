5d0bf7786af1f3f325d5da17edc67351ffef8a5f
David Petersen <david@familiep.de>
2020-01-08 12:57:22 +0100

    lambda lowering

--- src/main/kotlin/lambda/backend/Lowering.kt
+++ src/main/kotlin/lambda/backend/Lowering.kt
@@ -2,6 +2,8 @@ package lambda.backend
 
 import lambda.Lexer
 import lambda.Parser
+import lambda.Span
+import lambda.Type
 import lambda.syntax.DataConstructor
 import lambda.syntax.Expression
 import lambda.syntax.Name
@@ -11,13 +13,27 @@ import lambda.backend.Expression as IRExpression
 
 fun main() {
     val input = """
-        let y = 1 in 
-        (\x. add x y) 4
+        let y = 1 in
+        let z = 2 in 
+        (\x. add z y) 4
     """.trimIndent()
 
     val expr = Parser(Lexer(input)).parseExpression()
-    val lowering = Lowering(emptyMap())
+    val lowering =
+        Lowering(
+            mapOf(
+                Name("Tuple") to listOf(
+                    DataConstructor(
+                        Name("T"),
+                        listOf(Type.Int, Type.Int),
+                        Span.DUMMY
+                    )
+                )
+            )
+        )
     println(lowering.lowerExpr(expr, emptyMap()))
+    println("---")
+    println(lowering.liftedDeclarations)
 }
 
 class Lowering(private val typeTable: Map<Name, List<DataConstructor>>) {
@@ -32,21 +48,38 @@ class Lowering(private val typeTable: Map<Name, List<DataConstructor>>) {
             } ?: LnName.Free(expr.name))
 
             is Expression.Lambda -> {
-                val (args, body) = expr.foldArguments()
+                var (args, body) = expr.foldArguments()
                 val topName = freshName(Name("lifted"))
-                val freeVars = expr.freeVars().mapNotNull {
-                    env[it]?.let { index ->
-                        it to LnName.Bound(index)
-                    }
+                val capturedVars = expr.freeVars().mapNotNull { name ->
+                    env[name]?.let { name to it }
                 }
-                println("$topName: $freeVars")
-                IRExpression.Var(LnName.Free(topName)) //TODO
+                val newEnv = HashMap<Name, LnName.Index>()
+                args = capturedVars.map { it.first } + args
+                args.forEachIndexed { index, name ->
+                    newEnv[name] = LnName.Index(0, index)
+                }
+                val loweredBody = lowerExpr(body, newEnv)
+                liftedDeclarations += Declaration(topName, args, loweredBody)
+
+                capturedVars.map { it.second }
+                    .fold<LnName.Index, IRExpression>(IRExpression.Var(LnName.Free(topName))) { acc, ix ->
+                        IRExpression.App(
+                            acc,
+                            IRExpression.Var(LnName.Bound(ix))
+                        )
+                    }
             }
             is Expression.App -> IRExpression.App(lowerExpr(expr.func, env), lowerExpr(expr.arg, env))
             is Expression.Typed -> lowerExpr(expr.expr, env)
-            is Expression.Let -> if (expr.recursive) TODO() else {
-                val tempEnv = HashMap(env)
-                tempEnv.mapValues { it.value.shift() }
+            is Expression.Let -> if (expr.recursive) {
+                if (expr.expr !is Expression.Lambda)
+                    throw Exception("only functions may be defined recursively")
+
+
+                TODO()
+            } else {
+                val tempEnv = HashMap<Name, LnName.Index>()
+                env.mapValuesTo(tempEnv) { it.value.shift() }
                 tempEnv[expr.binder] = LnName.Index(0, 0)
                 IRExpression.Let(expr.binder, lowerExpr(expr.expr, env), lowerExpr(expr.body, tempEnv))
             }
@@ -65,14 +98,14 @@ class Lowering(private val typeTable: Map<Name, List<DataConstructor>>) {
     }
 
     private fun lowerCase(case: Expression.Case, env: Map<Name, LnName.Index>): IRExpression.Case {
-        val tempEnv = HashMap(env)
-        tempEnv.mapValues { it.value.shift() }
+        val tempEnv = HashMap<Name, LnName.Index>()
+        env.mapValuesTo(tempEnv) { it.value.shift() }
         case.binders.forEachIndexed { index, name -> tempEnv[name] = LnName.Index(0, index) }
         return IRExpression.Case(tagForDtor(case.type, case.dtor), case.binders, lowerExpr(case.body, tempEnv))
     }
 
     private fun tagForDtor(type: Name, dtor: Name): Int = typeTable.getValue(type).indexOfFirst { it.name == dtor } + 1
 
-    fun freshName(name: Name) = Name("$name${freshSupply++}")
+    fun freshName(name: Name) = Name("\$$name${freshSupply++}")
 }
 
